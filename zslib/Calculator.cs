f15a2e9862382f77efdc2ca2d536124c580040d5
zvonsri <93171322+zvonsri@users.noreply.github.com>
2023-07-15 15:44:57 -0400

    Calculator (#2)
    * Added Solution (#1)
    
    Co-authored-by: Sri <sgolyalla@gmail.com>
    
    * Initial Calculator
    
    ---------
    
    Co-authored-by: Sri <sgolyalla@gmail.com>

---
+++ zslib/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace zslib;
+public class Calculator
+{
+    public static int Add(int firstNum, int secondNum) => firstNum + secondNum;
+
+    public static int Subtract(int firstNum, int seconNum) => firstNum - seconNum;
+
+    public static int Multiply(int firstNum, int secondNum) => firstNum * secondNum;
+}
